8e4605bacfe972585165149dafd9d0dff30db818
SebastiaoBNJunior <junin_sbn@hotmail.com>
2023-06-21 21:08:57 -0300

    finalizado requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+const getEmployeeByName = (name) => {
+  if (!name) return {};
+
+  const employee = data.employees.find((emp) => emp.firstName === name || emp.lastName === name);
+
+  return employee
+    ? {
+      id: employee.id,
+      firstName: employee.firstName,
+      lastName: employee.lastName,
+      managers: employee.managers,
+      responsibleFor: employee.responsibleFor,
+    }
+    : {};
 };
 
 module.exports = getEmployeeByName;
