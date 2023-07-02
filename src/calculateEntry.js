9daf56c551bb20a4e705a060f99f50cbfa1e06ed
SebastiaoBNJunior <junin_sbn@hotmail.com>
2023-07-02 18:27:34 -0300

    finalizado requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aquii
-};
+const countEntrants = (entrants) => ({
+  child: entrants.filter((entrant) => entrant.age < 18).length,
+  adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length,
+  senior: entrants.filter((entrant) => entrant.age >= 50).length,
+});
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+  const r = countEntrants(entrants);
+  const b = data.prices;
+  const total = r.child * b.child + r.adult * b.adult + r.senior * b.senior;
+  return total;
 };
-
 module.exports = { calculateEntry, countEntrants };
