2184b282eb98813ddfbae0b525b52ef369a90193
SebastiaoBNJunior <junin_sbn@hotmail.com>
2023-07-03 23:20:09 -0300

    finalizado o requisito 10 na fé de Deus

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
-};
+  const findName = data.employees.find((people) => people.id === id).responsibleFor[0];
+  const findAnimal = data.species.find((animal) => animal.id === findName).residents;
 
+  let maxAge = 0;
+  let animalOld = 0;
+
+  findAnimal.forEach((animal) => {
+    if (animal.age > maxAge) {
+      maxAge = animal.age;
+      animalOld = animal;
+    }
+  });
+  return [`${animalOld.name}`, `${animalOld.sex}`, animalOld.age];
+};
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
