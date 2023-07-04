f43692beee532dd6079dc220ba315122cf1895f7
SebastiaoBNJunior <junin_sbn@hotmail.com>
2023-07-04 00:55:30 -0300

    finalizado o requisito 11 graça a deus santo papa e cristo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -15,5 +15,4 @@ const getOldestFromFirstSpecies = (id) => {
   });
   return [`${animalOld.name}`, `${animalOld.sex}`, animalOld.age];
 };
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
